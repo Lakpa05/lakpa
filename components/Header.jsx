6b21d662330d4694927186a3d7ee0b06e1e3f067
lakpao5 <lakpasherpa9865@gmail.com>
2026-04-07 10:56:18 +0545

    header  updated

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
                 <img src="/assets/hand-icon.png" alt="" className="w-6 mb-1" />
             </h3>
             <h1 className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo">frontend web developer based in Nepal.</h1>
-            <p className="max-w-2xl mx-auto font-Ovo">I am a frontend developer from Nepal with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.</p>
+            <p className="max-w-2xl mx-auto font-Ovo">I am a frontend developer from Nepal with 4 years of experience in multiple companies like SARA Computer & Service Pvt. Ltd. and Sathiko Sath Nepal.</p>
 
             <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
                 <a href="#contact"
