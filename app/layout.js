ed793f490e80e6b7edd8cb56c5474a644f8ff064
lakpao5 <lakpasherpa9865@gmail.com>
2026-04-19 09:25:01 +0545

    add Google Analytics integration to layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const outfit = Outfit({
     subsets: ["latin"],
@@ -21,6 +22,21 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className="font-Outfit leading-8 dark:bg-darkTheme dark:text-white">
+
+                {/* Google Analytics */}
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-0TBVN5QCES"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-0TBVN5QCES');
+                    `}
+                </Script>
+
                 {children}
             </body>
         </html>
